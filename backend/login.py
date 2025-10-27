f02af39fae5e722c0030ca5b01ce46946ee92c1f
Yongwoo Hur <2867744h@student.gla.ac.uk>
2025-10-27 17:25:16 +0000

    resolve to pull main

---
+++ backend/login.py
@@ -0,0 +1,9 @@
+# NOTE: NOT a priority task, focus on risk calculation and main app first
+
+'''
+TODO:
+1. Integrate postgres instance with user table
+2. Implement CRUD operation for user management
+3. Connect with UI login page
+4. (Not Priority) Add password hashing for security
+'''
