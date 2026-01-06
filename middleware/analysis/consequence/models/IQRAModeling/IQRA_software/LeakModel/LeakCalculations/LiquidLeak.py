b0dd927a2f9e5b3c7c006a13c9efcbd1aec64b81
Yongwoo Hur <2867744h@student.gla.ac.uk>
2026-01-06 17:20:48 +0000

    models update

--- middleware/analysis/consequence/models/IQRAModeling/IQRA_software/LeakModel/LeakCalculations/LiquidLeak.py
+++ middleware/analysis/consequence/models/IQRAModeling/IQRA_software/LeakModel/LeakCalculations/LiquidLeak.py
@@ -1,6 +1,4 @@
-import tkinter as tk
-from tkinter import messagebox
-import math
+import math #sqrt calculation
 
 class liquidLeakCalculator:
 
--- middleware/analysis/consequence/models/IQRAModeling/IQRA_software/LeakModel/LeakCalculations/TwoPhaseLeak.py
+++ middleware/analysis/consequence/models/IQRAModeling/IQRA_software/LeakModel/LeakCalculations/TwoPhaseLeak.py
@@ -1,5 +1,4 @@
-import tkinter as tk
-from tkinter import messagebox
+import math
 
 class twoLeakCalculator:
 
